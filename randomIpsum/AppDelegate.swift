802ac25f82745e2e8ddc6e0730c65416f785f389
Mike Rivera <rivera.mj@gmail.com>
2014-10-04 23:07:33 -0400

    4 space indent to 2 space indent

--- randomIpsum/AppDelegate.swift
+++ randomIpsum/AppDelegate.swift
@@ -10,73 +10,73 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  var pasteBoard = NSPasteboard.generalPasteboard()
+  var statusBarIcon = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+  
+  func sendToPasteBoard(text: String) {
+    pasteBoard.clearContents()
+    pasteBoard.writeObjects([text])
+  }
+  
+  func getLoremIpsum(requestedLength: String) {
+    let url = NSURL(string: "http://loripsum.net/api/" + requestedLength)
+    let request = NSURLRequest(URL: url!)
+    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+      self.sendToPasteBoard((NSString(data: data, encoding: NSUTF8StringEncoding) ?? "nope"))
+    }
+  }
+  
+  func oneLine(sender: NSMenuItem) {
+    getLoremIpsum("/1/short/plaintext")
+  }
+  
+  func twoLine(sender: NSMenuItem) {
+    getLoremIpsum("/2/short/plaintext")
+  }
+  
+  
+  func initMenu() {
+    var randomIpsumMenu = NSMenu()
+    var mainMenu = NSMenuItem()
     
-    var pasteBoard = NSPasteboard.generalPasteboard()
-    var statusBarIcon = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-
-    func sendToPasteBoard(text: String) {
-        pasteBoard.clearContents()
-        pasteBoard.writeObjects([text])
-    }
+    var languageMenu = NSMenu()
+    var languageMenuItem = NSMenuItem(title: "Choose Language", action: nil, keyEquivalent: "")
+    var latinLanugage = NSMenuItem(title: "Latin", action: nil, keyEquivalent: "")
     
-    func getLoremIpsum(requestedLength: String) {
-        let url = NSURL(string: "http://loripsum.net/api/" + requestedLength)
-        let request = NSURLRequest(URL: url!)
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-            self.sendToPasteBoard((NSString(data: data, encoding: NSUTF8StringEncoding) ?? "nope"))
-        }
-    }
+    latinLanugage.state = 1;
     
-    func oneLine(sender: NSMenuItem) {
-        getLoremIpsum("/1/short/plaintext")
-    }
+    languageMenu.addItem(latinLanugage)
     
-    func twoLine(sender: NSMenuItem) {
-        getLoremIpsum("/2/short/plaintext")
-    }
+    randomIpsumMenu.setSubmenu(languageMenu, forItem: languageMenuItem)
+    randomIpsumMenu.addItem(languageMenuItem)
     
+    randomIpsumMenu.addItem(NSMenuItem.separatorItem())
     
-    func initMenu() {
-        var randomIpsumMenu = NSMenu()
-        var mainMenu = NSMenuItem()
-
-        var languageMenu = NSMenu()
-        var languageMenuItem = NSMenuItem(title: "Choose Language", action: nil, keyEquivalent: "")
-        var latinLanugage = NSMenuItem(title: "Latin", action: nil, keyEquivalent: "")
-        
-        latinLanugage.state = 1;
-        
-        languageMenu.addItem(latinLanugage)
-        
-        randomIpsumMenu.setSubmenu(languageMenu, forItem: languageMenuItem)
-        randomIpsumMenu.addItem(languageMenuItem)
-        
-        randomIpsumMenu.addItem(NSMenuItem.separatorItem())
-        
-        var lineMenu = NSMenu()
-        var lineMenuItem = NSMenuItem(title: "Line", action: nil, keyEquivalent: "")
-        var oneLineItem = NSMenuItem(title: "1", action: Selector("oneLine:"), keyEquivalent: "")
-        var twoLineItem = NSMenuItem(title: "2", action: Selector("twoLine:"), keyEquivalent: "")
-
-        lineMenu.addItem(oneLineItem)
-        lineMenu.addItem(twoLineItem)
-        
-        randomIpsumMenu.setSubmenu(lineMenu, forItem: lineMenuItem)
-        randomIpsumMenu.addItem(lineMenuItem)
-        
-        self.statusBarIcon.menu = randomIpsumMenu
-    }
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        self.statusBarIcon.title = "RI"
-        
-        initMenu()
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
+    var lineMenu = NSMenu()
+    var lineMenuItem = NSMenuItem(title: "Line", action: nil, keyEquivalent: "")
+    var oneLineItem = NSMenuItem(title: "1", action: Selector("oneLine:"), keyEquivalent: "")
+    var twoLineItem = NSMenuItem(title: "2", action: Selector("twoLine:"), keyEquivalent: "")
+    
+    lineMenu.addItem(oneLineItem)
+    lineMenu.addItem(twoLineItem)
+    
+    randomIpsumMenu.setSubmenu(lineMenu, forItem: lineMenuItem)
+    randomIpsumMenu.addItem(lineMenuItem)
+    
+    self.statusBarIcon.menu = randomIpsumMenu
+  }
+  
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    self.statusBarIcon.title = "RI"
+    
+    initMenu()
+  }
+  
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+  
+  
 }
 
