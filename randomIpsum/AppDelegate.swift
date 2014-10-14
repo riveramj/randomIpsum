5ffe0434f6b0eded77fbae97dc244af4576e0405
Mike Rivera <rivera.mj@gmail.com>
2014-10-13 23:21:28 -0400

    switch on ipsum and text wanted

--- randomIpsum/AppDelegate.swift
+++ randomIpsum/AppDelegate.swift
@@ -11,29 +11,70 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   
+  enum IpsumType {
+    case Lorem
+    case Bacon
+  }
+  
+  enum TextType {
+    case Line
+    case Paragraph
+  }
+  
   var pasteBoard = NSPasteboard.generalPasteboard()
   var statusBarIcon = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-
+  var chosenIpsum = IpsumType.Lorem
+  var chosenIpsumURL = "http://loripsum.net/api/plaintext/"
   
   func sendToPasteBoard(text: String) {
     pasteBoard.clearContents()
     pasteBoard.writeObjects([text])
   }
   
-  func getLoremIpsum(requestedLength: String) {
-    let url = NSURL(string: "http://loripsum.net/api/" + requestedLength)
+  func getLoremIpsum(textAmount: Int, textType: TextType) {
+    var urlSuffix = ""
+
+    switch chosenIpsum {
+    case .Lorem:
+      switch textType {
+      case .Line:
+        urlSuffix = "\(textAmount)/short"
+      case .Paragraph:
+        urlSuffix = "\(textAmount)/medium"
+      }
+      
+    case .Bacon:
+      switch textType {
+      case .Line:
+        urlSuffix = "sentences=\(textAmount)"
+      case .Paragraph:
+        urlSuffix = "sentences=\(textAmount)"
+      }
+    }
+    
+    let url = NSURL(string: (chosenIpsumURL + urlSuffix))
     let request = NSURLRequest(URL: url!)
     NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
       self.sendToPasteBoard((NSString(data: data, encoding: NSUTF8StringEncoding) ?? "nope"))
     }
   }
   
-  func oneLine(sender: NSMenuItem) {
-    getLoremIpsum("/1/short/plaintext")
+  func loremIpsum(sender: NSMenuItem) {
+    chosenIpsum = .Lorem
+    chosenIpsumURL = "http://loripsum.net/api/plaintext/"
   }
   
-  func twoLine(sender: NSMenuItem) {
-    getLoremIpsum("/2/short/plaintext")
+  func baconIpsum(sender: NSMenuItem) {
+    chosenIpsum = .Bacon
+    chosenIpsumURL = "http://baconipsum.com/api/?type=all-meat&start-with-lorem=1&"
+  }
+  
+  func oneLine(sender: NSMenuItem) {
+    getLoremIpsum(1, textType: TextType.Line)
+  }
+  
+ func twoLine(sender: NSMenuItem) {
+    getLoremIpsum(2, textType: TextType.Line)
   }
   
   
@@ -43,11 +84,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var ipsumMenu = NSMenu()
     var ipsumMenuItem = NSMenuItem(title: "Ipsum", action: nil, keyEquivalent: "")
-    var loremIpsum = NSMenuItem(title: "Lorem", action: nil, keyEquivalent: "")
+    var loremIpsum = NSMenuItem(title: "Lorem", action: Selector("loremIpsum:"), keyEquivalent: "")
+    var baconIpsum = NSMenuItem(title: "Bacon", action: Selector("baconIpsum:"), keyEquivalent: "")
     
     loremIpsum.state = 1;
     
     ipsumMenu.addItem(loremIpsum)
+    ipsumMenu.addItem(baconIpsum)
     
     randomIpsumMenu.setSubmenu(ipsumMenu, forItem: ipsumMenuItem)
     randomIpsumMenu.addItem(ipsumMenuItem)
